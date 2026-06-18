df94a46d2ba45501e07f678e62a7e894b9464282
nicollynunessantos-spec <nicolly.nunes.santos@escola.pr.gov.br>
2026-06-18 12:44:28 +0000

    ok

--- app.py
+++ app.py
@@ -8,7 +8,17 @@ def início():
     curso = "Python com HTML"
 
     return render_template(
-        'index.html'
+        'index.html',
         nome = nome,
         curso = curso
     )
+
+    @app.route('/sobre')
+    def sobre():
+        return"""
+        <h1>sobre o Projeto</h1>
+        <p>Esse projeto foi criado usando Python e Flask.</p>
+        <a href="> Volta para o inicio</a>
+        """
+        if __name__=='__main__'
+         app.run(host='0.0.0.0', port=3000, debug=True)
